480493becfc5cceec1bae897e8a0df8eda868b5d
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-20 10:01:56 -0300

    feat:  create dependency generator function for FastAPI

--- app/core/database.py
+++ app/core/database.py
@@ -6,3 +6,7 @@ from app.core.config import settings
 engine =  create_async_engine(settings.DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 Base = declarative_base()
+
+async def get_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
