3e0918b97d0ad9cb66d817db1315397704372b91
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-20 10:44:58 -0300

    feat: create ( mapped ) class Room

--- app/models/booking.py
+++ app/models/booking.py
@@ -3,3 +3,12 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.sql import func
 from app.core.database import Base
 
+class Room(Base):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    total_capacity = Column(Integer, default=1)
+    available = Column(Integer, default=1)
+    version = Column(Integer, default= 0, nullable=False) #KEY FOR OPTIMISTIC LOCKING
+    
