d7137d87dfb539202a98b470e2fe222c1b9390e0
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-21 08:12:02 -0300

    feat: add method to set optimistic booking

--- app/services/booking_service.py
+++ app/services/booking_service.py
@@ -24,3 +24,35 @@ class BookingService:
             await db.rollback()
             return {"error": "No availability"}
         
+        
+    @staticmethod
+    async def optimistic_booking(db: AsyncSession, room_id: int, email:str):
+        # Read without locking
+        result = await db.execute(select(Room).where(Room.id == room_id))
+        room = result.scalar_one_or_none()
+
+        if not room:
+            return {"error": "Room not found"}
+        if room.available <= 0:
+            return {"error": "No availablity"}
+        
+        current_version = room.version
+
+        # Conditional update (WHERE version = :current_version)
+        stmt = (
+            update(Room)
+            .where(Room.id == room_id, Room.version == current_version)
+            .values(available=Room.available -1, version=Room.version + 1)
+        )
+        result_update = await db.execute(stmt)
+
+        if result_update.rowcount == 0:
+            # Conflict: someone modified the row before us
+            await db.rollback()
+            return {"error": "Concurrency conflict. Please retry."}
+        
+        # If updated succesfully, create the booking record
+        new_booking = Booking(room_id=room_id, user_email=email)
+        db.add(new_booking)
+        await db.commit()
+        return {"message": f"Booking confirmed for {email} (Optimistic)"}
