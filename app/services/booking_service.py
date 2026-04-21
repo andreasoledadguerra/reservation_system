83ca522726aff3c9ea4bc55b99dd812768172d3b
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-21 07:58:48 -0300

    feat: add method to set pessimistic booking

--- app/services/booking_service.py
+++ app/services/booking_service.py
@@ -1,3 +1,26 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
 from app.models.booking import Room, Booking
+
+class BookingService:
+
+    @staticmethod
+    async def pessimistic_booking(db: AsyncSession, room_id: int, email:str):
+        # Lock the row to prevent dirty reads
+        stmt = select(Room).where(Room.id == room_id).with_for_update()
+        result = await db.execute(stmt)
+        room = result.scalar_one_or_none()
+
+        if not room:
+            return {"error":"Room not found"}
+        
+        if room.available > 0:
+            room.available -= 1
+            new_booking = Booking(room_id, user_email=email)
+            db.add(new_booking)
+            await db.commit()
+            return {"message": f"Booking confirmed for {email}", "available": room.available}
+        else:
+            await db.rollback()
+            return {"error": "No availability"}
+        
