2c3ee02fc4d674f5d1034205604cb1b1762def13
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-21 09:14:55 -0300

    chore: import classes from fastapi, sqlalchemy, pydantic and app/

---
+++ app/api/v1/endpoints/bookings.py
@@ -0,0 +1,8 @@
+# pessimistic & optimistic endpoints
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.api import deps
+from app.services.booking_service import BookingService
+from pydantic import BaseModel
+
