2a2ec882f2c4e424735d97c0042fc6e713d03317
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-21 21:36:45 -0300

    feat: add @router for "optimistic" endpoint

--- app/api/v1/endpoints/bookings.py
+++ app/api/v1/endpoints/bookings.py
@@ -22,3 +22,16 @@ async def create_pessimistic_booking(
         raise HTTPException(status_code=400, detail=result["error"])
     return result
 
+
+@router.post("optimistic/{room_id}")
+async def create_optimistic_booking(
+    room_id: int,
+    payload: BookingRequest,
+    db: AsyncSession = Depends(deps.get_db)
+):
+    result = await BookingService.optimistic_booking(db, room_id, payload.email)
+    if "error" in result:
+        #Return 409 Conflict so tehe frontend can automatically  retry
+        status_code = 409 if "Conflict" in result["error"] else 400
+        raise HTTPException(status_code=status_code, detail=result["error"])
+    return result
