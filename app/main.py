2723880993d3bd3f766b5e8bac63375691ab3169
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-22 10:57:22 -0300

    feat: add @asynccontectmanager to the app

--- app/main.py
+++ app/main.py
@@ -3,8 +3,18 @@ from contextlib import asynccontextmanager
 from app.api.v1.endpoints import bookings
 from app.core.database import engine, Base
 
-app = FastAPI(title="Concurrent booking system")
 
 app.include_router(bookings.router, prefix="/api/v1/bookings", tags=["reservas"])
 
-#@asynccontextmanager 
+@asynccontextmanager 
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    print("Database initialized (tables created)")
+    
+    yield
+
+    await engine.dispose()
+    print("Database engine closed. Connections released.")
+
+
