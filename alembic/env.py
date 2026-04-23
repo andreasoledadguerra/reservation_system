8a01ba7867f83ea72758bf14a9a0f5e506c0b313
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-22 22:25:02 -0300

    feat: set environment variables from settings

--- alembic/env.py
+++ alembic/env.py
@@ -15,6 +15,9 @@ target_metadata = Base.metadata
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("+asyncpg", ""))
+
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
