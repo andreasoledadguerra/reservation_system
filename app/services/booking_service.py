ab7ea4b4d30096d7997f44a058e27afcd29512fb
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-28 22:21:16 -0300

    chore: update notebook

--- app/services/booking_service.py
+++ app/services/booking_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
-from app.models.booking import Room, Booking
+from app.models.room_model import Room, Booking
 
 class BookingService:
 
