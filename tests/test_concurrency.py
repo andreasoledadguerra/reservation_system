d8d5972a5765bb40f30cdcc1115202ae6b602cc6
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-04-30 12:59:35 -0300

    feat: add decorator for test for pessimistic concurrency

--- tests/test_concurrency.py
+++ tests/test_concurrency.py
@@ -22,4 +22,20 @@ async def setup_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         
+@pytest.mark.asyncio
+async def test_pessimistic_concurrency(setup_db):
+    room_1 = 1
+    async with AsynClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+        task = [
+            client.post(f"/api/v1/reservas/pessimistic/{room_1}", json={"email": f"user{i}@test.com"})
+            for i in range(10)
+        ]       
+        responses = await asyncio.gather(*task) #throw 10 requestes at the same time
+
+    sucess_count = sum(1 for r in responses if r.status_code == 200)
+    error_count = sum(1 for r in responses if r.status_code == 400)
+
+
+    assert sucess_count == 1, f"Only one booking must be successful, but it gets {sucess_count}"
+    assert error_count == 9, f"9 requests must fail, but {error_count} failed"
         
