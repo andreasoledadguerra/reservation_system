2ee08a73778413e070fb22e8840075d0769e39b0
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2026-05-06 09:29:17 -0300

    feat: instatiate asynchronous session factory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest_asyncio
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from app.core.database import engone, Base
+from app.core.database import engine, Base
 from app.models. booking import Room
 
+# Create an asynchronous session factory (using actual engine)
+async_session_factory = async_sessionmaker(engine, expire_on_commit=False)
